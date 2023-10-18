f4b396153fa3ccf91144ade6feba26678900782f
n4n5 <its.just.n4n5@gmail.com>
2023-10-18 18:20:11 +0200

    fix: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,8 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
     integrations: [mdx()],
-    site: 'https://lh.rezel.net',
-    experimental: {
-        redirects: true,
-    },
+    site: "https://lh.rezel.net",
     redirects: {
-        '/fr/': '/',
-    }
+        "/fr/": "/",
+    },
 });
