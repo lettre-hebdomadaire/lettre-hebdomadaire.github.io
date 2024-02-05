379214961603f97005984cd05c60e8acd6ec197b
n4n5 <its.just.n4n5@gmail.com>
2024-02-05 08:56:42 +0100

    fix: better footer

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,8 @@
 export const color = "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+
+export const links = {
+    fb: "https://www.facebook.com/profile.php?id=100091511889368",
+    upload: "https://partage.imt.fr/index.php/s/KnEQKityPBLdbo5",
+    discord: "https://discord.gg/jxjK9w5VKs",
+    mail: "lettre.hebdomadaire.telecom@gmail.com",
+};
